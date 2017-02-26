b5c03d716acf2256e62f43cf81d02d9c7131a203
avrohom goldstein <avrohom goldstein>
2017-02-26 17:45:59 -0500

    git commit all

---
+++ homework/hw_68/httpUpercaser.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var http = require('http'),
+    fs = require('fs'),
+    map = require('through2-map'),
+    server = http.createServer(function (req, res) {
+
+
+
+
+
+
+        req.pipe(map(function (chunk) {
+            return chunk.toString().toUpperCase();
+        })).pipe(res);
+
+    });
+server.listen(process.argv[2])
